d614b98f2117f385d6ae094a08b06cf873700e77
LordGoblin <maxime78600@gmail.com>
2019-01-20 18:46:54 +0100

    ok

---
+++ 16.01.2019rendu_jeudi/lib/crypto.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'xpath'
+   
+ def cryptop
+	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))   
+
+	hachier = {}
+	array = []
+	i = 0
+	crypto = page.xpath('//a[@class="link-secondary"]')
+	price = page.xpath('//a[@class="price"]')
+
+	crypto.each do |cryptotext|
+		hachier = {}
+		(hachier[cryptotext.text] = price[i].text[1..-1])
+	    array[i] = hachier
+	    i = i + 1
+	end
+
+
+	print array
+
+	return array
+end
+
+print cryptop
